2efbbe8532509c3ec67aad9629fd4888682515f9
mt60000 <niruyaku45@gmail.com>
2021-11-30 10:56:04 +0900

    Chapter0

---
+++ src/Chapter0/variable.java
@@ -0,0 +1,14 @@
+package Chapter0;
+
+public class variable {
+	public static void main(String[] args) {
+		System.out.println("すがわら");
+		System.out.println("31歳です");
+		System.out.println("お酒が好きです");
+		System.out.println("31 + 31の計算をします");
+		System.out.println(31 + 31);
+		int x;
+		x = 6;
+		System.out.println(x * x * 3.14);
+	}
+}
