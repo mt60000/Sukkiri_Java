f028dd1b4dde542a26e466b2faa36e3f08d0b2fb
mt60000 <niruyaku45@gmail.com>
2021-12-17 16:35:31 +0900

    Chapter8 finish

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,33 @@
 
 public class Main {
-
+	public static void main(String[] args) {
+		Hero h = new Hero();
+		h.name = "ミナト";
+		h.hp = 100;
+		
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
+		
+		Matango m2 = new Matango();
+		m2.hp = 48;
+		m2.suffix = 'B';
+		
+		Cleric c = new Cleric();
+		c.name = "もち";
+		
+		//冒険のはじまり
+		h.slip();
+		c.selfAid();
+		c.pray(3);
+		m1.run();
+		m2.run();
+		h.run();
+		
+//		System.out.println("勇者" + h.name + "を生み出しました！");
+//		h.sit(5);
+//		h.slip();
+//		h.sit(25);
+//		h.run();
+	}
 }
