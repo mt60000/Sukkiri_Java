85575736d7d2eb406739c6bf6491733fdd16ddd2
mt60000 <niruyaku45@gmail.com>
2022-01-10 14:18:28 +0900

    practice

--- src/practice/Practice5.java
+++ src/practice/Practice5.java
@@ -1,5 +1,20 @@
 package practice;
 
 public class Practice5 {
-
+	public static void main(String[] args) {
+		// weight == 60;
+		//(age1 + age2) * 2 > 60;
+		// (age % 2) == 1;
+		// name.equals("湊");  ←文字の一致に関する条件式
+		int isHungry = 1;
+		String food = "mochi";
+		System.out.println("こんにちは");
+		if(isHungry == 0) {
+			System.out.println("お腹がいっぱいです");
+		} else {
+			System.out.println("はらぺこです");
+			System.out.println(food + "をいただきます");
+			System.out.println("ごちそうさまでした");
+		}
+	}
 }
---
+++ src/practice/Practice8.java
@@ -0,0 +1,23 @@
+package practice;
+
+public class Practice8 {
+	public static void main(String[] args) {
+//		int[] points = new int[4]; 
+//		double[] weights = new double[5];
+//		boolean[] answers = new boolean[3];
+//		String[] names = new String[3];
+		
+	/*	int[] moneyList = new int[3];
+		moneyList[0] = 121902;
+		moneyList[1] = 8302;
+		moneyList[2] = 55100;
+	*/
+		int[] moneyList = {121902, 8302, 55100};
+		for (int i = 0; i < moneyList.length; i++) {
+			System.out.println(moneyList[i]);
+		}
+		for (int m : moneyList) {
+			System.out.println(m);
+		}
+	}
+}
